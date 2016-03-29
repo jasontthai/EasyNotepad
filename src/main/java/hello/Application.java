db8526fb99293d46fb864599dba16ffcda521757
Jason Thai <trit@yahoo-inc.com>
2016-03-29 15:32:23 -0700

    add jsondoc dependency

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,11 +2,13 @@ package hello;
 
 import java.util.Arrays;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableJSONDoc
 public class Application {
 
     public static void main(String[] args) {
